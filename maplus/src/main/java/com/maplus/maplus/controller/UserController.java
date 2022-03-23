4f2218e261f6e6ae9153f9b0705d690659754dea
PINO <912609664@qq.com>
2022-03-24 01:08:35 +0800

    Update login

--- maplus/src/main/java/com/maplus/maplus/controller/UserController.java
+++ maplus/src/main/java/com/maplus/maplus/controller/UserController.java
@@ -14,12 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
-    
+    private UserService userService;
+
     //用户注册
     @PostMapping(value = "userRegister")
-    public boolean saveUser(@RequestBody User user) {
+    public Integer saveUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
 
+    //用户登录
+    @PostMapping(value = "login")
+    public boolean login(@RequestBody User user){
+        return userService.login(user);
+    }
+
 }
+
+
+
