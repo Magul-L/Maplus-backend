0621571c3c5ce347847dd9d6602c53c059119d82
hickey317 <66119010+hickey317@users.noreply.github.com>
2022-03-27 21:03:59 +0800

    Update UserController.java

--- maplus/src/main/java/com/maplus/maplus/controller/UserController.java
+++ maplus/src/main/java/com/maplus/maplus/controller/UserController.java
@@ -20,6 +20,11 @@ public class UserController {
 
     private UserService userService;
 
+    //用户名查重
+    @PostMapping(value = "userCheck")
+    public Integer checkUser(@RequestBody String userName){
+        return userService.checkUser(userName);
+    }
     //用户注册
     @PostMapping(value = "userRegister")
     public Integer saveUser(@RequestBody User user) {
