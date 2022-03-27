3c9361355bcae01fd68eb938b12df8b862d45482
hickey317 <66119010+hickey317@users.noreply.github.com>
2022-03-27 21:05:57 +0800

    Update UserService.java

--- maplus/src/main/java/com/maplus/maplus/service/UserService.java
+++ maplus/src/main/java/com/maplus/maplus/service/UserService.java
@@ -3,7 +3,7 @@ package com.maplus.maplus.service;
 import com.maplus.maplus.model.User;
 
 public interface UserService {
-
+    public Integer checkUser(String user);
     public Integer saveUser(User user);
     public boolean login(User user);
     public boolean changePsw(String userName, String userPassword,String newPassword);
