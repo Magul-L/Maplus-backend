d59696a222b3968696ae22360fbe85ffbbb978ef
zhongwen <1286668885@qq.com>
2022-04-27 09:54:34 +0800

    test: comments

--- maplus/src/main/java/com/maplus/maplus/repo/CommentRepo.java
+++ maplus/src/main/java/com/maplus/maplus/repo/CommentRepo.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface CommentRepo extends JpaRepository<Comment,Integer> {
 
-    @Query(value = "SELECT c.user_name as user_name, c.comtext as comtext FROM comment as c WHERE c.activityid = :actID ORDER BY c.date_time ",nativeQuery = true)
+    @Query(value = "SELECT c.user_name as user_name, c.comtext as comtext, c.date_time as date_time FROM comment as c WHERE c.activityid = :actID ORDER BY c.date_time ",nativeQuery = true)
     List<Object> findByActivityID(int actID);
 
 }
